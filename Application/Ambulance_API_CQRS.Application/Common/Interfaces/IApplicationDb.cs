1f68f090726a21b323e9d92cc021fa9fea51361c
dovgalev92 <dovgalev2021@inbox.ru>
2023-11-15 00:07:30 +0300

    interfaces IAplicationDb

---
+++ Application/Ambulance_API_CQRS.Application/Common/Interfaces/IApplicationDb.cs
@@ -0,0 +1,16 @@
+﻿using Ambulance_API_CQRS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambulance_API_CQRS.Application.Common.Interfaces
+{
+    public interface IApplicationDb
+    {
+        DbSet<Patient> Patients { get; }
+        DbSet<CallingAmbulance> Callings { get; }
+        DbSet<AmbulanceDepart> Departs { get; }
+        DbSet<Locality> Localities { get; }
+        DbSet<Street> Streets { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellation = default);
+    }
+}
