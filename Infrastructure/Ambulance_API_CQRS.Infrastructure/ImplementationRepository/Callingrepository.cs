10c2d36b1d558cb83a656530d7a9eb3c909ad103
dovgalev92 <dovgalev2021@inbox.ru>
2023-11-26 21:09:36 +0300

    AddCallingRepos

---
+++ Infrastructure/Ambulance_API_CQRS.Infrastructure/ImplementationRepository/Callingrepository.cs
@@ -0,0 +1,20 @@
+﻿using Ambulance_API_CQRS.Application.Common.Interfaces;
+using Ambulance_API_CQRS.Application.Common.Interfaces.CallingAmbual;
+using Ambulance_API_CQRS.Domain.Entities;
+
+namespace Ambulance_API_CQRS.Infrastructure.ImplementationRepository
+{
+    public class Callingrepository : ICallingRepository
+    {
+        private readonly IApplicationDb _application;
+        public Callingrepository(IApplicationDb application)
+        {
+            _application = application;
+        }
+        public async Task<int> CreateCalling(CallingAmbulance create)
+        {
+            await _application.Callings.AddAsync(create);
+            return create.Id;
+        }
+    }
+}
---
+++ Infrastructure/Ambulance_API_CQRS.Infrastructure/ImplementationRepository/PatientRepos.cs
@@ -0,0 +1,33 @@
+﻿using Ambulance_API_CQRS.Application.Common.Interfaces;
+using Ambulance_API_CQRS.Application.Common.Interfaces.PatientRepository;
+using Ambulance_API_CQRS.Domain.Entities;
+
+
+namespace Ambulance_API_CQRS.Infrastructure.ImplementationRepository
+{
+    public class PatientRepos : IPatientRepos
+    {
+        private readonly IApplicationDb _application;
+        public PatientRepos(IApplicationDb application)
+        {
+            _application = application;
+        }
+
+        public async Task<int> CreatePatient(Patient patient)
+        {
+            await _application.Patients.AddAsync(patient);
+            return patient.Id;
+        }
+
+        public Task<IEnumerable<Patient>> GetAllPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<Patient> GetPatientById(int patientid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
