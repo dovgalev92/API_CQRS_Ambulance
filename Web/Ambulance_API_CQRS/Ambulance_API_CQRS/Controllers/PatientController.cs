26b1ccfb6112ad59e5687bd4300920fceec94f25
dovgalev92 <dovgalev2021@inbox.ru>
2024-01-23 23:51:17 +0300

    Add GetPatientId and update action AddPatient, GetPatients

--- Web/Ambulance_API_CQRS/Ambulance_API_CQRS/Controllers/PatientController.cs
+++ Web/Ambulance_API_CQRS/Ambulance_API_CQRS/Controllers/PatientController.cs
@@ -1,9 +1,12 @@
-﻿using Ambulance_API_CQRS.Application.Patients.Command.CreatePatient;
+﻿using Ambulance_API_CQRS.Application.Patients;
+using Ambulance_API_CQRS.Application.Patients.Command.CreatePatient;
 using Ambulance_API_CQRS.Application.Patients.Queries.GetAllPatient;
 using Ambulance_API_CQRS.Application.Patients.Queries.GetPatientDetails;
+using Ambulance_API_CQRS.Application.Patients.Queries.GetPatientId;
 using Ambulance_API_CQRS.Models.DTO.PatientDTO;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace Ambulance_API_CQRS.Controllers
 {
@@ -21,26 +24,40 @@ namespace Ambulance_API_CQRS.Controllers
             var map = _mapper.Map<CreatePatientCommand>(create);
             var patientId = await Mediator.Send(map);
 
-            return Ok(patientId);
+            return CreatedAtAction(nameof(GetPatientId), new { id = patientId }, map);
         }
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<GetAllPatientVm>> GetPatient()
+        public async Task<IActionResult> GetPatient([FromQuery] PatientParametrDto parametr)
         {
-            var query = new GetAllPatientsQuery();
+            var patientParametr = _mapper.Map<PatientParametr>(parametr);
+            var query = await Mediator.Send(new GetAllPatientsQuery() { Parametr = patientParametr });
+            Response.Headers.Add("X-Pagination", query.GetMetadate());
 
-            return Ok(await Mediator.Send(query));
+            var result = _mapper.Map<IEnumerable<GetAllPatientDto>>(query);
+
+            return Ok(result);
 
         }
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<GetPatientDetailsDto>> GetPatientId(int id)
+        public async Task<ActionResult<GetPatientDetailsDto>> GetPatientDetails(int id)
         {
             var query = new GetPatientDetailsQuery()
             {
                 Id = id
             };
             return Ok(await Mediator.Send(query));
+
+        }
+        [HttpGet("get-patient-id/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ActionName(nameof(GetPatientId))]
+        public async Task<ActionResult<GetPatientIdDto>> GetPatientId(int id)
+        {
+            var query = new GetPatientIdQuery() { Id = id };
+            var send = await Mediator.Send(query);
+            return Ok(send);
 
         }
 
