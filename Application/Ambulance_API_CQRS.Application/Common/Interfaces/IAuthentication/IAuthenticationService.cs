cfa27ed91f931ea8498c82b4924d25b834d5326d
dovgalev92 <dovgalev2021@inbox.ru>
2024-03-19 23:38:10 +0300

    Add ServiceAuthentication

---
+++ Application/Ambulance_API_CQRS.Application/Common/Interfaces/IAuthentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ambulance_API_CQRS.Application.Common.Interfaces.IAuthentication
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser();
+        Task<bool> ValidateUser();
+        Task<string> CreateToken();
+    }
+}
